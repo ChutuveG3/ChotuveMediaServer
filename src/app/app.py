cb7e1befb2a7f24cdb075aa23967a6bfd927f578
bauticanavese <bauti-canavese@hotmail.com>
2020-05-03 21:30:44 -0300

    config mongodb

--- src/app/app.py
+++ src/app/app.py
@@ -1,6 +1,7 @@
 import os
 import logging
 from flask import Flask, escape, request, jsonify
+from pymongo import MongoClient
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -10,6 +11,11 @@ LOG_FORMAT = '%(levelname)s %(asctime)s - %(message)s'
 logging.basicConfig(level=logging.DEBUG,
                     format=LOG_FORMAT)
 logger = logging.getLogger()
+
+# Config. database and set default.
+client = MongoClient(os.getenv('MONGO_URL', 'mongodb://localhost:27017/db'))
+db = client.get_database(os.getenv('DB_NAME', 'test'))
+logger.info(f'Connected to DB: {db.name}')
 
 
 @app.route('/', methods=['GET'])
--- src/tests/test_example.py
+++ src/tests/test_example.py
@@ -1,8 +1,10 @@
 import unittest
 
+
 class TestExample(unittest.TestCase):
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
+
 
 if __name__ == '__main__':
     unittest.main()
