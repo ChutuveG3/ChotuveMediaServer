0c58aa240cfa32e38f961448400fb2b1f6c11ddb
bauticanavese <bauti-canavese@hotmail.com>
2020-05-11 21:48:15 -0300

    set default datetime

--- src/app/models/video.py
+++ src/app/models/video.py
@@ -1,8 +1,14 @@
 from mongoengine import *
+import datetime
+
+def _not_empty(val):
+    if not val:
+        raise ValidationError()
+
 
 
 class Video(Document):
-    file_name = StringField()
-    file_size = IntField()
-    download_url = StringField()
-    datetime = DateTimeField()
+    file_name = StringField(validation=_not_empty)
+    file_size = IntField(validation=_not_empty, min_value=0)
+    download_url = StringField(validation=_not_empty)
+    datetime = DateTimeField(validation=_not_empty, default=datetime.datetime.utcnow)
--- src/tests/test_add_video.py
+++ src/tests/test_add_video.py
@@ -1,14 +1,11 @@
 import unittest
-
 from src.app.models.video import Video
 
 
 class TestVideoModel(unittest.TestCase):
-    def test_video_filename(self):
+    def test_to_default_datetime(self):
         video = Video(file_name='file_name_test')
-        self.assertEqual(video.file_name, None)
-        self.assertEqual(video.datetime, None)
-        self.assertEqual(video.download_url, None)
+        self.assertIsNotNone(video.datetime)
 
 
 if __name__ == '__main__':
