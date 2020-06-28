ebd0ebc91dbcd6abcb908af5edf0e0cba1da0108
bauticanavese <bauti-canavese@hotmail.com>
2020-06-28 19:39:51 -0300

    add validate app server api key

--- src/app/services/auth_service.py
+++ src/app/services/auth_service.py
@@ -19,3 +19,13 @@ class AuthService:
             raise AuthorizationError()
 
         return Response({}, 200)
+
+    @staticmethod
+    def validate_app_server(api_key):
+        url = os.environ.get('AUTH_BASE_URL', "") + '/connect/api_key_validation'
+        r = requests.get(url, headers={'x_api_key': api_key})
+
+        if r.status_code != 200:
+            raise AuthenticationError()
+
+        return Response({}, 200)
--- src/tests/services/test_auth_service.py
+++ src/tests/services/test_auth_service.py
@@ -22,16 +22,30 @@ class TestAuthService(unittest.TestCase):
         self.assertEqual(r.status_code, 200)
 
     @patch('app.services.auth_service.requests.get')
-    def test_check_admin_token_with_invalid_token(self, mock_get):
+    def test_validate_admin_token_with_invalid_token(self, mock_get):
         with self.assertRaises(AuthenticationError):
-            mock_get.side_effect = Mock(status_code=401)
+            mock_get.return_value = Mock(status_code=401)
             AuthService.validate_admin_token('invalid_token')
 
     @patch('app.services.auth_service.requests.get')
-    def test_check_admin_token_not_admin(self, mock_get):
+    def test_validate_admin_token_not_admin(self, mock_get):
         with self.assertRaises(AuthorizationError):
             mock_get.return_value = Mock(status_code=200, json=lambda: {"privilege": False})
             AuthService.validate_admin_token('fake_token_not_admin')
+
+    @patch('app.services.auth_service.requests.get')
+    def test_validate_app_server_api_key_success(self, mock_get):
+        mock_get.return_value = Mock(status_code=200, json=lambda: {})
+        r = AuthService.validate_app_server('fake_api_key')
+
+        self.assertEqual(mock_get.call_count, 1)
+        self.assertEqual(r.status_code, 200)
+
+    @patch('app.services.auth_service.requests.get')
+    def test_validate_app_server_api_key_fail(self, mock_get):
+        with self.assertRaises(AuthenticationError):
+            mock_get.return_value = Mock(status_code=403)
+            AuthService.validate_app_server('fake_api_key')
 
 
 if __name__ == '__main__':
