ee8a486a94ef2a50ac6bdfe4b122b97a9275cdd9
bauticanavese <bauti-canavese@hotmail.com>
2020-07-02 16:09:43 -0300

    allow OPTION method on CORS

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -72,5 +72,5 @@ def auth_before_request():
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     return response
